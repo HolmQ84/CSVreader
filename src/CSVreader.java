cf634746395446e4286e5c1b8ada5d53f42c3788
HolmQ84 <badboy2287>
2020-05-19 09:42:38 +0200

    New commit

--- src/CSVreader.java
+++ src/CSVreader.java
@@ -45,7 +45,7 @@ public class CSVreader {
             }
             try (FileWriter fileWriter = new FileWriter("actorList.csv")) {
                 for (Movie current: movieList) {
-                    fileWriter.write(current.getTitel()+","+current.getYear()+","+current.getImdb()+","+current.getDirector()+","+current.getActors()+"\n");
+                    fileWriter.write(current.getTitel()+";"+current.getYear()+";"+current.getImdb()+";"+current.getDirector()+";"+current.getActors()+"\n");
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -59,12 +59,21 @@ public class CSVreader {
     public void searchIntructor(String name) {
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             int counter = 1;
+            movieList.add(new Movie("Titel", "Årstal", "IMDB-score", "Instruktør","Skuespillere"));
             while ((line = br.readLine()) != null) {
                 String[] movie = line.split(splitter);
                 if (movie[1].contains(name)) {
+                    movieList.add(new Movie(movie[11],movie[23],movie[25],movie[1],(movie[6]+" & "+movie[10]+" & "+movie[14])));
                     System.out.println(counter+" -\u001B[34m Filmtitel: \u001B[0m"+movie[11]+",\u001B[31m Årstal: \u001B[0m"+movie[23]+",\u001B[32m IMDB-score: \u001B[0m"+movie[25]+" ,\u001B[33m Intruktør: \u001B[0m"+movie[1]+" ,\u001B[35m Skuespillere: \u001B[0m"+movie[6]+", "+movie[10]+" & "+movie[14]+".");
                     counter++;
                 }
+            }
+            try (FileWriter fileWriter = new FileWriter("instructorList.csv")) {
+                for (Movie current: movieList) {
+                    fileWriter.write(current.getTitel()+";"+current.getYear()+";"+current.getImdb()+";"+current.getDirector()+";"+current.getActors()+"\n");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
